704d56693733ec2564dec734cd21a4a6d6ef0fa2
chengfanbrain <chengfanbrain@gmail.com>
2023-09-23 19:42:53 +0900

    update output directory

--- feature_decoding-to-generator.sh
+++ feature_decoding-to-generator.sh
@@ -6,29 +6,33 @@ PARAM1="./feature-decoding/config/deeprecon-fmd-mscoco_fmriprep_500voxel_caffe_b
 PARAM2="./feature-decoding/config/deepreconS6-fmd-mscoco_fmriprep_500voxel_caffe_bvlc_reference_caffenet_allunits_fastl2lir_alpha100.yaml"
 
 ## decoder training
+# output: results/feature-decoding/ImageNetTraining_FMD_MSCOCO
 
 # subject S1-5, S7
-python feature-decoding/featdec_fastl2lir_train.py $PARAM1
+python feature-decoding/featdec_fastl2lir_train.py $PARAM1 
 # subject S6
-python feature-decoding/featdec_fastl2lir_train.py $PARAM2
+python feature-decoding/featdec_fastl2lir_train.py $PARAM2 
 
 ## predict decoded features
-# single trial
+# output: results/feature-decoding/Illusion_single_trial
+
 # subject S1-5, S7
 python feature-decoding/featdec_fastl2lir_predict.py $PARAM1
 # subject S6
-python feature-decoding/featdec_fastl2lir_predict.py $PARAM2
+python feature-decoding/featdec_fastl2lir_predict.py $PARAM2 
 
 
 
 ### step 2: reconstruction - feed decoded features to generator
+# output: results/reconstruction/recon_images
 
 ## GAN
-python generator/recon_feature_to_GAN.py
+python generator/recon_feature_to_GAN.py  
 
 
 
 ### step 3: visualization
 
-## GAN
-python visualization/make_figures_recon_images.py
+# output: results/plots/Fig2.pdf
+python visualization/make_figures_recon_images.py 
+
--- feature_decoding-to-generator_quick_test.sh
+++ feature_decoding-to-generator_quick_test.sh
@@ -6,6 +6,7 @@ PARAM1="./feature-decoding/config/deeprecon-fmd-mscoco_fmriprep_500voxel_caffe_b
 PARAM2="./feature-decoding/config/deepreconS6-fmd-mscoco_fmriprep_500voxel_caffe_bvlc_reference_caffenet_allunits_fastl2lir_alpha100_quick_test.yaml"
 
 ## decoder training
+# output: results/feature-decoding/ImageNetTraining_FMD_MSCOCO
 
 # subject S1-5, S7
 python feature-decoding/featdec_fastl2lir_train.py $PARAM1
@@ -13,7 +14,8 @@ python feature-decoding/featdec_fastl2lir_train.py $PARAM1
 python feature-decoding/featdec_fastl2lir_train.py $PARAM2
 
 ## predict decoded features
-# single trial
+# output: results/feature-decoding/Illusion_avg_trials
+
 # subject S1-5, S7
 python feature-decoding/featdec_fastl2lir_predict.py $PARAM1
 # subject S6
@@ -22,13 +24,14 @@ python feature-decoding/featdec_fastl2lir_predict.py $PARAM2
 
 
 ### step 2: reconstruction - feed decoded features to generator
+# output: results/reconstruction/recon_images
 
-## GAN
+## GAN_quick_test
 python generator/recon_feature_to_GAN_quick_test.py
 
 
 
 ### step 3: visualization
 
-## GAN
+# output: results/plots/qucik_test
 python visualization/make_figures_recon_images_quick_test.py
